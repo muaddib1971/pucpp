e3060a1fc78cf6d45437860a8ec0c91c53e1975c
Paul Miller <paul@researchbook.muaddib>
2017-04-04 21:36:38 +1000

    source code examples for week 6 chat

---
+++ chat_examples/week6/widget.cpp
@@ -0,0 +1,43 @@
+#include <iostream>
+#include <cstdlib>
+#include <memory>
+
+struct widget
+{
+    int size;
+    std::unique_ptr<widget[]> widgets;
+
+    widget(void) : size(0), widgets(nullptr){}
+    explicit widget(int _size) : size(_size), 
+        widgets (std::make_unique<widget[]>(_size)){}
+
+    widget(const widget& w) 
+    {
+        widgets = std::make_unique<widget[]>(w.size);
+        int count;
+        size = w.size;
+        for(count = 0; count < size; ++count)
+        {
+            /* do the copying of the widget */
+        }
+    }
+
+    widget operator=(const widget& other)
+    {
+        /* do the same stuff as in the copy constructor */
+        widget w(other);
+        return w;
+    }
+
+    virtual ~widget()
+    {
+    }
+};
+
+int main(void)
+{
+    std::string s = "hello";
+    /* string(const char * array) */
+    widget a(3);
+    widget b=a;
+}
