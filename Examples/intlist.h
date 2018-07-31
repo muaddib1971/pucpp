52f8935433c0474cdaf977f8ca2dd47bca4c4da2
xiangminemilyzhou <xiangmin.zhou@rmit.edu.au>
2018-07-31 10:05:51 +1000

    Add files via upload

---
+++ Examples/intlist.h
@@ -0,0 +1,26 @@
+#include <memory>
+#include <iostream>
+class linked_list
+{
+    class node
+    {
+        int data;
+        std::unique_ptr<node> next;
+        public:
+        node(int newdata) : data(newdata), next(nullptr) {}
+
+        void set_next(std::unique_ptr<node>&& newnext);
+        node * get_next(void);
+        int get_data(void);
+        std::unique_ptr<node>& get_next_ptr(void);
+        friend class linked_list;
+    };
+
+    std::unique_ptr<node>head;
+    int size;
+    public:
+    linked_list(void) : head(nullptr), size(0) {}
+    bool add(int);
+    bool search(int);
+    void print(void);
+};
